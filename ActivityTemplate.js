c38526377b0cff609955a35eb0ee154965968b17
sarahlalayin <146173831+sarahlalayin@users.noreply.github.com>
2026-05-05 13:27:09 +0800

    Delete ActivityTemplate.js

--- ActivityTemplate.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const activityTemplateSchema = new mongoose.Schema({
-  category: { type: String, required: true }, // e.g. "運動", "才藝", "家事"
-  title: { type: String, required: true }, // e.g. "跳繩500下", "練鋼琴30分鐘"
-  defaultPoints: { type: Number, default: 10 }
-}, { timestamps: true });
-
-module.exports = mongoose.model('ActivityTemplate', activityTemplateSchema);
