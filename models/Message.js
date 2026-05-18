98822d1df516243059ea6b6243efea61087cb63a
sarahlalayin <146173831+sarahlalayin@users.noreply.github.com>
2026-05-18 10:18:12 +0800

    Add files via upload

--- models/Message.js
+++ models/Message.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
   familyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Family', required: true },
   text: { type: String, required: true },
-  from: { type: String, enum: ['parent', 'system'], default: 'parent' }
+  from: { type: String, enum: ['parent', 'student', 'system'], default: 'parent' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Message', messageSchema);
